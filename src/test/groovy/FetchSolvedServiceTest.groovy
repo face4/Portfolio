9accefdf9393ff6dad26219e14872273dcb3d6b1
face4 <s2020583@u.tsukuba.ac.jp>
2020-10-08 11:39:39 +0900

    introduce Spock

---
+++ src/test/groovy/FetchSolvedServiceTest.groovy
@@ -0,0 +1,31 @@
+import com.app.portfolio.Model.Aoj
+import com.app.portfolio.Model.Stat
+
+import com.app.portfolio.Service.FetchSolvedService
+import org.springframework.web.client.RestTemplate
+import spock.lang.Specification
+
+class FetchSolvedServiceTest extends Specification{
+
+    // テスト対象のクラス
+    private static FetchSolvedService service;
+
+    def setup(){
+        service = new FetchSolvedService()
+        service.restTemplate = Mock(RestTemplate)
+    }
+
+    def "test #1"(){
+        given:
+            def AojEntity = new Aoj()
+            AojEntity.with {
+                setStatus(new Stat())
+                status.setSolved(11111)
+            }
+        when:
+            def retVal = service.getAojSolved()
+        then:
+            1 * service.restTemplate.getForObject(_, _) >> AojEntity
+            retVal == 11111
+    }
+}
